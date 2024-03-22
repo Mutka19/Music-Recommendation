8fae8a932be0dd19f07ccffe8f5355fe9e902527
dmutka <daemon.mutka98@gmail.com>
2024-03-22 15:03:20 -0500

    Changed id type to UUID

--- model/person.py
+++ model/person.py
@@ -1,13 +1,9 @@
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
 from repository.db_model import db
 
 
 class Person(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(32), unique=True, nullable=False)
-
-    def __repr__(self):
-        return "<User %r>" % self.username
-
-    def validate(self, password):
-        return self.password == password
--- model/song_record.py
+++ model/song_record.py
@@ -1,8 +1,10 @@
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
 from repository.db_model import db
 
 
 class SongRecord(db.Model):
-    id = db.Column(db.uuid, primary_key=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
     username = db.Column(db.String(80), unique=False, nullable=False)
     artist = db.Column(db.String(300), unique=False, nullable=False)
     song = db.Column(db.String(300), unique=False, nullable=False)
