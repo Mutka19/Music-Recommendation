f03f5a12bb6e802ec16dab2440ad4fb12c4973f6
dmutka <daemon.mutka98@gmail.com>
2024-03-24 19:25:48 -0500

    Removed nullable=False from password column

--- model/person.py
+++ model/person.py
@@ -7,7 +7,7 @@ from repository.db_model import db
 class Person(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(32), unique=False, nullable=False)
+    password = db.Column(db.String(32), unique=False)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
