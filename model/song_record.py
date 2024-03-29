d857b49bcfbc8c73807bbc2f28ae3a47a1e2687f
dmutka <daemon.mutka98@gmail.com>
2024-03-29 12:20:15 -0500

    Updated SongRecord structure

--- model/song_record.py
+++ model/song_record.py
@@ -5,6 +5,8 @@ from repository.db_model import db
 
 class SongRecord(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
-    username = db.Column(db.String(80), unique=False, nullable=False)
     artist = db.Column(db.String(300), unique=False, nullable=False)
     song = db.Column(db.String(300), unique=False, nullable=False)
+    album = db.Column(db.String(300), unique=False)
+    release_date = db.Column(db.DateTime, unique=False)
+    person_id = db.Column(UUID(as_uuid=True), db.ForeignKey("person.id"), nullable=False)
